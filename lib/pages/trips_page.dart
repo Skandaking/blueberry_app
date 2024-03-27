2e8d97b223e442c303a03cb9fa2281a1aff7ad1a
Skandaking <skandakaunda@gmail.com>
2024-03-28 01:47:13 +0200

    changed flight trip added taps to the main pages

--- lib/pages/trips_page.dart
+++ lib/pages/trips_page.dart
@@ -1,13 +1,56 @@
+import 'package:blueberry_app/componets/side_navbar.dart';
 import 'package:flutter/material.dart';
 
 class TripsPage extends StatelessWidget {
-  const TripsPage({super.key});
+  const TripsPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Center(child: Text('trips page')),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: SideNavbar(),
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          title: Center(child: Text('My Trips')),
+          actions: [
+            IconButton(icon: Icon(Icons.history), onPressed: () {}),
+          ],
+        ),
+        body: Column(
+          children: [
+            TabBar(
+              // Moving TabBar outside app bar
+              tabs: [
+                Tab(text: 'Past'), // Tab for One way
+                Tab(text: 'Upcoming'), // Tab for Round trip
+              ],
+            ),
+            Expanded(
+              // Use Expanded to allow the TabBarView to take remaining space
+              child: TabBarView(
+                // Adding TabBarView to switch between tabs
+                children: [
+                  // Content of the first tab (One way)
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: Text('Past trips..'),
+                    ),
+                  ),
+                  // Content of the second tab (Round trip)
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: Text('Upcoming trips...'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
